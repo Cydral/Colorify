0e04f150f3c4f60ac8f7a7bafcd2e22325608fc3
Cydral <53169060+Cydral@users.noreply.github.com>
2023-09-18 13:34:09 +0200

    Add files via upload

--- ffmpeg_video_muxing_ex.cpp
+++ ffmpeg_video_muxing_ex.cpp
@@ -54,35 +54,6 @@ void scale_image(long nr, long nc, matrix<pixel_type>& dst) {
     assign_image(dst, resized);
 }
 
-// ----------------------------------------------------------------------------------------
-template <typename GeneratorType>
-matrix<rgb_pixel> generate_image(GeneratorType& net, const matrix<gray_pixel>& src) {
-    static_assert(std::is_same_v<GeneratorType, lr_generator_type> || std::is_same_v<GeneratorType, hr_generator_type>,
-        "net must be either lr_generator_type or hr_generator_type");
-
-    matrix<gray_pixel> gray_image = src;
-    resize_inplace(gray_image, std_image_size);
-    matrix<rgb_pixel> rgb_image, blur_image;
-    if constexpr (std::is_same_v<GeneratorType, lr_generator_type>) {
-        matrix<uint16_t> output = net(gray_image);
-        rgb_image = concat_channels(gray_image, output);
-        scale_image(src.nr(), src.nc(), rgb_image);
-    }
-    else if constexpr (std::is_same_v<GeneratorType, hr_generator_type>) {
-        std::array<matrix<float>, 2> output = net(gray_image);
-        rgb_image = concat_channels(gray_image, output);
-    }
-    gaussian_blur(rgb_image, blur_image);
-    scale_image(src.nr(), src.nc(), blur_image);
-    matrix<lab_pixel> lab_image;
-    assign_image(lab_image, blur_image);
-    for (long r = 0; r < lab_image.nr(); ++r)
-        for (long c = 0; c < lab_image.nc(); ++c)
-            lab_image(r, c).l = src(r, c);
-    assign_image(rgb_image, lab_image);
-    return rgb_image;
-}
-
 int main(const int argc, const char** argv)
 try {
     command_line_parser parser;
@@ -96,7 +67,7 @@ try {
     parser.add_option("help", "display this message and exit");
     parser.add_option("height", "height of encoded stream (defaults to whatever is in the video file)", 1);
     parser.add_option("width", "width of encoded stream (defaults to whatever is in the video file)", 1);
-    parser.add_option("video-codec", "video codec name (e.g. \"h264\")", 1);
+    parser.add_option("video-codec", "video codec name (e.g. \"mpeg4\")", 1);
     parser.add_option("audio-codec", "audio codec name (e.g. \"aac\")", 1);
 
     parser.parse(argc, argv);
@@ -121,21 +92,22 @@ try {
 
     // Check which DNN model to use for the colorization process and load the model
     bool high_resolution_model = parser.option("low-resolution") ? false : true;
-    lr_generator_type lr_generator;
-    hr_generator_type hr_generator;
+    const string model_name = high_resolution_model ? "highres_colorify.dnn" : "lowres_colorify.dnn";
+    using net_type_lr = loss_multiclass_log_per_pixel_weighted<cont<256, 1, 1, 1, 1, generator_backbone<input<matrix<gray_pixel>>>>>;
+    using net_type_hr = loss_mean_squared_per_channel_and_pixel<2, cont<2, 1, 1, 1, 1, generator_backbone<input<matrix<gray_pixel>>>>>;
+    net_type_lr net_lr;
+    net_type_hr net_hr;
     if (high_resolution_model) {
-        if (file_exists("highres_colorify.dnn")) {
-            deserialize("highres_colorify.dnn") >> hr_generator;
-        } else {
-            cout << "Didn't find the model (highres_colorify.dnn)" << endl;
+        if (file_exists(model_name)) deserialize(model_name) >> net_hr;
+        else {
+            cout << "Didn't find the model (" << model_name << ")" << endl;
             cout << "This model can be downloaded here: https://github.com/Cydral/Colorify/highres_colorify.zip\n";
             return EXIT_FAILURE;
         }
     } else {
-        if (file_exists("lowres_colorify.dnn")) {
-            deserialize("lowres_colorify.dnn") >> lr_generator;
-        } else {
-            cout << "Didn't find the model (lowres_colorify.dnn)" << endl;
+        if (file_exists(model_name)) deserialize(model_name) >> net_lr;
+        else {
+            cout << "Didn't find the model (" << model_name << ")" << endl;
             cout << "This model can be downloaded here: https://github.com/Cydral/Colorify/lowres_colorify.zip\n";
             return EXIT_FAILURE;
         }
@@ -180,8 +152,8 @@ try {
     cout << "Frame rate: " << cap.fps() << " fps" << endl;
 
     frame f;
-    matrix<gray_pixel> gray_image;
-    matrix<rgb_pixel> input_image, gen_image;
+    matrix<gray_pixel> gray_image, temp_gray_image;
+    matrix<rgb_pixel> input_image, rgb_image, blur_image;
     uint64_t processed_samples = 0;
 
     dlib::image_window win;
@@ -192,19 +164,40 @@ try {
         if (f.is_image()) {
             convert(f, input_image);
             rgb_image_to_grayscale_image(input_image, gray_image);
-            gen_image = high_resolution_model ? generate_image(hr_generator, gray_image) : generate_image(lr_generator, gray_image);
-            convert(gen_image, f);
+            // ---
+            {
+                assign_image(temp_gray_image, gray_image);
+                resize_inplace(temp_gray_image, std_image_size);
+                if (high_resolution_model) {
+                    std::array<matrix<float>, 2> output = net_hr(temp_gray_image);
+                    rgb_image = concat_channels(temp_gray_image, output);
+                } else {
+                    matrix<uint16_t> output = net_lr(temp_gray_image);
+                    rgb_image = concat_channels(temp_gray_image, output);
+                }
+                gaussian_blur(rgb_image, blur_image, 0.7);
+                scale_image(gray_image.nr(), gray_image.nc(), blur_image);
+                matrix<lab_pixel> lab_image;
+                assign_image(lab_image, blur_image);
+                for (long r = 0; r < lab_image.nr(); ++r)
+                    for (long c = 0; c < lab_image.nc(); ++c)
+                        lab_image(r, c).l = gray_image(r, c);
+                assign_image(rgb_image, lab_image);
+            }
+            // ---
+            convert(rgb_image, f);
             resize_inplace(input_image, std_image_size);
-            resize_inplace(gen_image, std_image_size);
-            win.set_image(join_rows(input_image, gen_image));          
+            resize_inplace(rgb_image, std_image_size);
+            win.set_image(join_rows(input_image, rgb_image));
         }
         writer.push(std::move(f));
         if ((++processed_samples % 100) == 0) {
-            long progress_percentage = static_cast<long>((static_cast<double>(processed_samples) / cap.estimated_nframes() * 100.0f));
+            long progress_percentage = static_cast<long>((static_cast<double>(processed_samples) / (cap.estimated_nframes() * 3) * 100.0f));
             std::cout << "Colorization progress: [" << string(long(progress_percentage / 2), '=') << "] " << progress_percentage << "%" << "\r";
             std::cout.flush();
         }
     }
+    std::cout << "Colorization progress: [" << string(long(100 / 2), '=') << "] " << 100 << "%" << "\n";
     cout << "Conversion done, flushing on disk... ";
     writer.flush();
     cout << "done" << endl;
